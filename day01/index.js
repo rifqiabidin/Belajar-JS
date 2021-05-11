71c00bb5a824fc5e0d137bbc27b22499fff258b1
Rifqi <fiqirrifqi@gmail.com>
2021-05-11 16:34:58 +0700

    commit day 2

---
+++ day01/index.js
@@ -0,0 +1,35 @@
+// variable
+// string
+
+// const nama = 'yangki';
+
+// console.log(nama.length);
+// console.log('string: ')
+// console.log(nama[5]);
+// console.log('charAt: ')
+// console.log(nama.charAt(5));
+// console.log('concat: ')
+// console.log(nama.concat(' siapa?'));
+// console.log('toUpperCase: ')
+// console.log(nama.concat(' siapa?').toUpperCase());
+
+
+// mengubah tipe data
+
+// let angka = 8;
+// console.log(angka);
+// console.log(typeof angka);
+
+// let strAngka = String(angka);
+// console.log(strAngka);
+// console.log(typeof strAngka);
+
+// let angkaPecahan = '8'
+// console.log(parseFloat(angkaPecahan));
+
+// Operator
+    // matematika
+    // perbandingan
+    // kondisional
+
+const nama = (nama_)
---
+++ day02/index.js
@@ -0,0 +1,20 @@
+// const tokoBuka = true;
+// const uang = 50000;
+
+// uang > 50000 ? console.log("sugih") : console.log("kere");
+
+const tombol = 0;
+
+switch(tombol){
+    case 1: {
+        console.log(`tombol ${tombol} ditekan`);
+        break;
+    }
+    case 2: {
+        console.log(`tombol ${tombol} ditekan`);
+        break;
+    }
+    default:{
+        console.log(`tidak ada tombol ditekan`)
+    }
+}
