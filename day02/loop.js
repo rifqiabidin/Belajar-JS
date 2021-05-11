8d4db4458171d78f8a1576373b5c4f41892baca5
Rifqi <fiqirrifqi@gmail.com>
2021-05-11 16:47:57 +0700

    loop day02

---
+++ day02/loop.js
@@ -0,0 +1,13 @@
+// while loop
+// let status = `puasa`;
+
+// while (status == `puasa`){
+//     console.log(`laper ey`);
+//     status = `buka`;
+// }
+
+// for loop
+
+// for (let i=0; i < 10; i+= 2){
+//     console.log(i)
+// }
