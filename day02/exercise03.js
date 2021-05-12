15bc41d876c3c5f97c4008ad5a23ee82042955b5
Rifqi <fiqirrifqi@gmail.com>
2021-05-12 15:45:31 +0700

    adding exercise03

--- day02/exercise03.js
+++ day02/exercise03.js
@@ -11,3 +11,39 @@
 //     2 3
 //     3 4 5
 //     5 6 7 8
+
+// soal nomor 1
+
+// let angka = 73;
+
+// if (1 <= angka <= 100) {
+//     if (angka%2 === 0) {
+//         console.log(`angka ${angka} adalah bilangan genap`)
+//     } else {
+//         console.log(`angka ${angka} adalah bilangan ganjil`)
+//     }
+// } else {
+//     console.log('angka berada di luar rentang 1-100')
+// }
+
+// soal nomor 2
+// let angka = 5;
+
+// for (let index = 1; index <= angka; index++) {
+//     console.log('#'.repeat(index));
+// }
+
+// soal nomor 3
+
+let baris = 4;
+let angka = 1;
+let temp = "";
+
+for (let index = 1; index <= baris; index++) {
+    temp = "";
+    for (let index2 = 1; index2 <= index; index2++) {
+        temp = temp+" "+angka;
+        angka++;        
+    }
+    console.log(temp);    
+}
